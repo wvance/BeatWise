c63d679a665ab3321f5e48385b14ad7e69872a46
Wesley Vance <wesley.vance@mac.com>
2015-12-12 17:25:54 -0600

    Fixed nav overflow

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -1,8 +1,7 @@
 class ChannelController < ApplicationController
+  before_action :set_identities
   def index
     if user_signed_in?
-      @identities = current_user.identities.all
-
       @twitter = @identities.where(:provider => "twitter")
       @foursquare = @identities.where(:provider => "foursquare")
       @github = @identities.where(:provider => "github")
@@ -11,4 +10,15 @@ class ChannelController < ApplicationController
       @instagram = @identities.where(:provider => 'instagram')
     end
   end
+  def twitter
+    @twitter = @identities.where(:provider => "twitter")
+    if @twitter.present?
+      @userTweets = current_user.contents.order('created_at DESC').where(:provider => "twitter")
+    end
+  end
+
+  private
+    def set_identities
+      @identities = current_user.identities.all
+    end
 end

54b21c3f581c63623ba4fc04f08fe74a9984c4c7
Wesley Vance <wesley.vance@mac.com>
2016-04-12 00:21:25 -0500

    Continued to improve on memory

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if user_signed_in?
-      @alluserContent = current_user.contents.all
+      @alluserContent = current_user.contents.count
 
       unless @alluserContent.present?
         redirect_to channels_path, notice:"Please Sign on and pull some data!"
@@ -16,80 +16,80 @@ class WelcomeController < ApplicationController
 
       # FOR THE MAP :D
       # GET ALL CONTENT OBJECTS FOR THE MAP DISPLAY
-      @mapContent = @alluserContent
-      # THIS IS FOR THE DISPLAY MAP
-      @geojson = Array.new
-      # raise @mapContent.to_yaml
-      # PUT CONTENTS ON MAP
-      if @mapContent.present?
-        @mapContent.each do |content|
-          unless (content.longitude.nil? || content.latitude.nil?) || (content.longitude == '0' || content.latitude == '0')
+      # @mapContent = @alluserContent
+      # # THIS IS FOR THE DISPLAY MAP
+      # @geojson = Array.new
+      # # raise @mapContent.to_yaml
+      # # PUT CONTENTS ON MAP
+      # if @mapContent.present?
+      #   @mapContent.each do |content|
+      #     unless (content.longitude.nil? || content.latitude.nil?) || (content.longitude == '0' || content.latitude == '0')
 
-            if (content.provider == "twitter")
-              marker_color = '#4099FF'
-            elsif (content.provider == "foursquare")
-              marker_color = '#FFCC00'
-            elsif (content.provider == "facebook")
-              marker_color = '#FFFFFF'
-            elsif (content.provider == "fitbit")
-              marker_color = '#f15038'
-            elsif (content.provider == "github")
-              marker_color = '#F1AC38'
-            else
-              marker_color = "#387C81"
-            end
+      #       if (content.provider == "twitter")
+      #         marker_color = '#4099FF'
+      #       elsif (content.provider == "foursquare")
+      #         marker_color = '#FFCC00'
+      #       elsif (content.provider == "facebook")
+      #         marker_color = '#FFFFFF'
+      #       elsif (content.provider == "fitbit")
+      #         marker_color = '#f15038'
+      #       elsif (content.provider == "github")
+      #         marker_color = '#F1AC38'
+      #       else
+      #         marker_color = "#387C81"
+      #       end
 
-            @geojson << {
-              type: 'Feature',
-              geometry: {
-                type: 'Point',
-                coordinates: [
-                  content.longitude,
-                  content.latitude
-                ]
-              },
-              properties: {
-                title:
-                  if (content.title.present?)
-                    content.title.capitalize
-                  elsif (content.provider.present? && content.kind.present? && !content.body.present? )
-                    content.provider.capitalize + " " + content.kind.capitalize
-                  elsif (content.provider.present?)
-                    content.provider.capitalize
-                  else
-                    " "
-                  end,
-                body:
-                  if (content.body.present?)
-                    content.body.capitalize
+      #       @geojson << {
+      #         type: 'Feature',
+      #         geometry: {
+      #           type: 'Point',
+      #           coordinates: [
+      #             content.longitude,
+      #             content.latitude
+      #           ]
+      #         },
+      #         properties: {
+      #           title:
+      #             if (content.title.present?)
+      #               content.title.capitalize
+      #             elsif (content.provider.present? && content.kind.present? && !content.body.present? )
+      #               content.provider.capitalize + " " + content.kind.capitalize
+      #             elsif (content.provider.present?)
+      #               content.provider.capitalize
+      #             else
+      #               " "
+      #             end,
+      #           body:
+      #             if (content.body.present?)
+      #               content.body.capitalize
 
-                  elsif (content.kind.present?)
-                    content.kind.capitalize + " on " + content.created_at.strftime("%b %e, %Y")
-                  else
-                    " "
-                  end,
-                external_link:
-                    content_path(content.id),
-                    # "http://blackboxapp.io/content/"+ content.id.to_s,
-                image:
-                  if (content.image.present?)
-                    content.image
-                  else
-                    " "
-                  end,
-                address:
-                  if (content.city.present? && content.state.present?)
-                    content.city + ", " + content.state
-                  elsif content.location.present?
-                    content.location
-                  end,
-                :'marker-color' => marker_color,
-                :'marker-size' => 'small'
-              }
-            }
-          end
-        end
-      end
+      #             elsif (content.kind.present?)
+      #               content.kind.capitalize + " on " + content.created_at.strftime("%b %e, %Y")
+      #             else
+      #               " "
+      #             end,
+      #           external_link:
+      #               content_path(content.id),
+      #               # "http://blackboxapp.io/content/"+ content.id.to_s,
+      #           image:
+      #             if (content.image.present?)
+      #               content.image
+      #             else
+      #               " "
+      #             end,
+      #           address:
+      #             if (content.city.present? && content.state.present?)
+      #               content.city + ", " + content.state
+      #             elsif content.location.present?
+      #               content.location
+      #             end,
+      #           :'marker-color' => marker_color,
+      #           :'marker-size' => 'small'
+      #         }
+      #       }
+      #     end
+      #   end
+      # end
     end
     respond_to do |format|
       format.html
